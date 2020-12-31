57aeaede191e3361fdb7b02bc18cf106df929445
Maxim Oransky <maxim.oransky@gmail.com>
2020-12-31 18:05:46 +0300

    libvps

---
+++ Dockerfile
@@ -0,0 +1,34 @@
+FROM ubuntu:18.04
+
+ENV LANG en_US.UTF-8
+ENV LC_ALL en_US.UTF-8
+
+RUN apt-get update && apt-get upgrade -y \
+    && DEBIAN_FRONTEND=noninteractive apt-get install -y locales tzdata \
+    && sed -i -e 's/# en_US.UTF-8 UTF-8/en_US.UTF-8 UTF-8/' /etc/locale.gen \
+    && dpkg-reconfigure --frontend=noninteractive locales \
+    && update-locale LANG=en_US.UTF-8
+
+RUN apt-get update && apt-get install -y --no-install-recommends \
+    automake \
+    build-essential \
+    ca-certificates \
+    cmake \
+    curl \
+    debconf \
+    debhelper \
+    dpkg \
+    git \
+    gnupg \
+    gobject-introspection \
+    libexpat1-dev \
+    libglib2.0-dev \
+    mc \
+    pkg-config \
+    software-properties-common \
+    sudo \
+    wget
+
+WORKDIR /root/builder
+
+CMD ["tail", "-F", "/dev/null"]
---
+++ libvips/build.sh
@@ -0,0 +1,35 @@
+#!/usr/bin/env bash
+set -ex
+cd `dirname $0`
+
+readonly LIBVPS_VERSION=8.10.5
+readonly SRC=https://github.com/libvips/libvips/releases/download/v${LIBVPS_VERSION}/vips-${LIBVPS_VERSION}.tar.gz
+readonly DEST=td-libvips-dev_${LIBVPS_VERSION}
+readonly DEB=${DEST}.deb
+
+rm -rf ${DEST}
+mkdir -p ${DEST}
+
+cp -rv DEBIAN.template ${DEST}/DEBIAN
+sed -i "s/%LIBVPS_VERSION%/${LIBVPS_VERSION}/g" ${DEST}/DEBIAN/*
+
+(
+    mkdir -p ${DEST}/usr/local/src
+    cd ${DEST}/usr/local/src
+    curl -L -o vips-${LIBVPS_VERSION}.tar.gz ${SRC}
+    tar zvxf vips-${LIBVPS_VERSION}.tar.gz
+    rm vips-${LIBVPS_VERSION}.tar.gz
+)
+(
+    dpkg-deb --build ${DEST}
+)
+
+# echo "Try install ${DEB}"
+# if dpkg -i ${DEB}; then
+#     echo "Installed"
+# else
+#     echo "Fix requirements"
+#     apt-get -f install -y
+# fi
+
+mv ${DEB} ../dist
