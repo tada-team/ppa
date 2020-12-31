8902b9993d2263a1426ea1aab65e5164627f1528
Maxim Oransky <maxim.oransky@gmail.com>
2020-12-31 18:11:30 +0300

    update

---
+++ update-dist.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+set -ex
+cd `dirname $0`
+
+EMAIL=maxim.oransky@gmail.com
+cd dist
+
+# Packages & Packages.gz
+dpkg-scanpackages --multiversion . > Packages
+gzip -k -f Packages
+
+# Release, Release.gpg & InRelease
+apt-ftparchive release . > Release
+gpg --default-key "${EMAIL}" -abs -o - Release > Release.gpg
+gpg --default-key "${EMAIL}" --clearsign -o - Release > InRelease
+
+git add -A
+git commit -m "update dist"
+git push
